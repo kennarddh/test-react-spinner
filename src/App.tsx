5ee36e5b02a24b551a8ff72836bdb5406088cd7d
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-08 20:01:52 +0700

    feat: Generate random color

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 import Spinner from '@kennarddh/react-spinner'
 import '@kennarddh/react-spinner/build/style.css'
 
+const GenerateRandomHexColor = () =>
+	`#${Math.floor(Math.random() * 16777215).toString(16)}`
+
 const App: FC = () => {
+	const [Color, SetColor] = useState<string>(GenerateRandomHexColor)
+	const [BackgroundColor, SetBackgroundColor] = useState<string>(
+		GenerateRandomHexColor,
+	)
+
 	return (
 		<div>
 			<Spinner
-				backgroundColor='blue'
-				color='red'
+				backgroundColor={BackgroundColor}
+				color={Color}
 				fullRotationTime={1}
 				height={100}
 				outlineWidth={10}
 				width={100}
 			/>
+			<button onClick={() => SetColor(GenerateRandomHexColor())}>
+				Change Color
+			</button>
+			<button
+				onClick={() => SetBackgroundColor(GenerateRandomHexColor())}
+			>
+				Change Background Color
+			</button>
 		</div>
 	)
 }
