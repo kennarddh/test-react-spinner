17866cbfa0f97c12bf43edc2daad9b2d67cefaea
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-08 19:53:58 +0700

    feat: Use @kennarddh/react-spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import { FC } from 'react'
 
+import Spinner from '@kennarddh/react-spinner'
+import '@kennarddh/react-spinner/build/style.css'
+
 const App: FC = () => {
-	return <div></div>
+	return (
+		<div>
+			<Spinner
+				backgroundColor='blue'
+				color='red'
+				fullRotationTime={1}
+				height={100}
+				outlineWidth={10}
+				width={100}
+			/>
+		</div>
+	)
 }
 
 export default App
